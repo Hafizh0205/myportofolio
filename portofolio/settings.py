9151f68945ca3c1aaecffc33d5c43cd2dcccf411
Hafizh Zuhdi <Hafzhproboy12@gmail.com>
2026-09-01 20:37:39 +0700

    Fix TEMPLATES backend Configuration

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -48,11 +48,12 @@ ROOT_URLCONF = 'portofolio.urls'
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.DjangoTemplates',
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',  # <-- Perhatikan huruf besar/kecilnya
         'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
