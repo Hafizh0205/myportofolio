65f44c2470fb4d14637d86214b1fbb0bd1e87939
Hafizh Zuhdi <Hafzhproboy12@gmail.com>
2026-09-08 21:15:27 +0700

    update

--- main/apps.py
+++ main/apps.py
@@ -1,5 +1,18 @@
 from django.apps import AppConfig
 
-
 class MainConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'main'
+
+    def ready(self):
+        try:
+            from main.models import Experience
+            if not Experience.objects.exists():
+                Experience.objects.create(
+                    title="Asisten Dosen PBP",
+                    description="Membantu mahasiswa memahami dasar pengembangan web.",
+                    category="part-time",
+                    is_ongoing=True
+                )
+        except Exception:
+            pass
