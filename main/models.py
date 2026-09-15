c0fc52c2e968dc5f03d05647f42496772ce5c5d3
Hafizh Zuhdi <Hafzhproboy12@gmail.com>
2026-09-16 01:37:42 +0700

    Complete tutorial 03

--- main/models.py
+++ main/models.py
@@ -1,3 +1,4 @@
+import uuid
 from django.db import models
 
 class Experience(models.Model):
@@ -14,10 +15,12 @@ class Experience(models.Model):
         return self.title
 
 class Project(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=255)
     description = models.TextField()
     tech_stack = models.CharField(max_length=255)
     project_url = models.URLField(blank=True, null=True)
+    project_image_url = models.URLField(blank=True, null=True, max_length=500)
 
     def __str__(self):
         return self.title
